aa5547ca9b5cb32f66e6761ed2448f93bdf42e39
mac10688 <mac10688@gmail.com>
2016-01-02 20:49:52 -0600

    Adding unit tests

--- Chapter16/16_17_exercises.hs
+++ Chapter16/16_17_exercises.hs
@@ -51,15 +51,12 @@ instance Functor (Company e e') where
 
 --3.
 
-newtype Flip f a b =
-    Flip (f b a)
-    deriving (Eq, Show)
+data More b a = L a b a | R b a b deriving (Eq, Show)
 
-data More a b = L a b a | R b a b deriving (Eq, Show)
+instance Functor (More b) where
+    fmap f (L a b a') = L (f a) b (f a')
+    fmap f (R b a b') = R b (f a) b'
 
-instance Functor (Flip More a) where
-    fmap f (Flip (L a b a')) = Flip $ L (f a) b (f a')
-    fmap f (Flip (R b a b')) = Flip $ R b (f a) b'
 
 --Prelude> fmap (+1) (L 1 2 3)
 --L 2 2 4
@@ -68,41 +65,81 @@ instance Functor (Flip More a) where
 
 --Write Functor instances for the following datatypes.
 --1.
---data Quant a b = Finance | Desk a | Bloor b
+data Quant a b = Finance | Desk a | Bloor b
+
+instance Functor (Quant a) where
+    fmap _ Finance = Finance
+    fmap f (Desk a) = Desk a
+    fmap f (Bloor b) = Bloor (f b)
 
 --2. No, it's not interesting by itself.
---data K a b = K a
+data K a b = K a
+
+instance Functor (K a) where
+    fmap f (K a) = K a
 
 --3. {-# LANGUAGE FlexibleInstances #-}
 --newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
 
---newtype L' a b = L' a
+newtype L' a b = L' a
+
+newtype Flip f a b =
+    Flip (f b a)
+    deriving (Eq, Show)
 
 -- should remind you of an instance you've written before
---instance Functor (Flip L' a) where
---    fmap = undefined
+instance Functor (Flip L' a) where
+    fmap f (Flip (L' a)) = Flip $ L' (f a)
 
 --4. No, it doesn't do anything interesting. No magic here or in the previous exercise. If it works, you succeeded.
---data EvilGoateeConst a b = GoatyConst b
+data EvilGoateeConst a b = GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+    fmap f (GoatyConst b) = GoatyConst (f b)
 
 --5. Do you need something extra to make the instance work?
---data LiftItOut f a = LiftItOut (f a)
+data LiftItOut f a = LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+    fmap f' (LiftItOut fa) = LiftItOut (fmap f' fa)
 
 --6.
---data Parappa f g a = DaWrappa (f a) (g a)
+data Parappa f g a = DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+    fmap f' (DaWrappa fa ga) = DaWrappa (fmap f' fa) (fmap f' ga)
 
 --7. Don't ask for more typeclass instances than you need. You can let GHC tell you what to do.
---data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+
+instance (Functor g) => Functor (IgnoreOne f g a) where
+    fmap f (IgnoringSomething fa gb) = IgnoringSomething fa (fmap f gb)
 
 --8.
---data Notorious g o a t = Notorious (g o) (g a) (g t)
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance (Functor g) => Functor (Notorious g o a) where
+    fmap f (Notorious go ga gt) = Notorious go ga (fmap f gt)
 
 --9. You'll need to use recursion.
---data List a = Nil | Cons a (List a)
+data List a = Nil | Cons a (List a)
+
+instance Functor List where
+    fmap f (Nil) = Nil
+    fmap f (Cons a la) = Cons (f a) (fmap f la) 
 
 --10.
---data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor GoatLord where
+    fmap _ NoGoat = NoGoat
+    fmap f (OneGoat a) = OneGoat (f a)
+    fmap f (MoreGoats fa fb fc) = MoreGoats (fmap f fa) (fmap f fb) (fmap f fc)
 
 --11.
---data TalkToMe a = Halt | Print String a | Read (String -> a)
+data TalkToMe a = Halt | Print String a | Read (String -> a)
 
+instance Functor TalkToMe where
+    fmap _ Halt = Halt
+    fmap f (Print x a) = Print x (f a)
+    fmap f (Read fa) = Read (fmap f fa)
